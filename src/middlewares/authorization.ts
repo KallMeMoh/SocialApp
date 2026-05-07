aec3a4cb30f9c00805b9ffa74abec4235ab48c97
KallMeMoh <kallmemoh@gmail.com>
2026-05-07 22:00:47 +0300

    Implement authorization middleware.

---
+++ src/middlewares/authorization.ts
@@ -0,0 +1,12 @@
+import type { NextFunction, Request, Response } from 'express';
+import { HttpError } from '../common/errors/http-error.js';
+import { UserRole } from '../common/types/user.type.js';
+
+export const authorize =
+  (authorizedRoles = UserRole.User) =>
+  (req: Request, _res: Response, next: NextFunction) => {
+    if (authorizedRoles !== req.userRole)
+      throw new HttpError(403, "You don't have enough permissions");
+
+    next();
+  };
